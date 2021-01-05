704ef560c5c8166c44996e1c95fd4062edfc43f2
SuDex <sudnishnikoff@gmail.com>
2021-01-05 13:05:22 +0300

    Initial commit

---
+++ src/test/java/by/rexy/voting/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.rexy.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
